6ce080eebc886fb51fd679b41135695477989b68
Vangasse <vangasse@ic.ufal.br>
2021-02-28 20:56:36 -0300

    tunning, setpoint parameter correction

--- matlab/MPC/denso_nlmpc.m
+++ matlab/MPC/denso_nlmpc.m
@@ -3,37 +3,37 @@ ny = 14;
 nu = 6;
 nlobj = nlmpc(nx,ny,nu);
 
-Ts = 0.5;
-p = 20;
+%% MPC Parameters
+Ts = 0.1;
+p = 40;
 c = 10;
 nlobj.Ts = Ts;
 nlobj.PredictionHorizon = p;
 nlobj.ControlHorizon = c;
 
+%% Model Functions
 nlobj.Model.StateFcn = "densoStateFunction";
 nlobj.Model.IsContinuousTime = true;
-% nlobj.Model.NumberOfParameters = 1;
-
-
 nlobj.Jacobian.StateFcn = "densoJacobianFunction";
 
-
+%% Model Weights
 nlobj.Weights.OutputVariables = [ones(1, 8), zeros(1, 6)];
 nlobj.Weights.ManipulatedVariablesRate = [0*ones(1, 6)];
 
 nlmpcobj.Optimization.UseSuboptimalSolution = 1;
 
+%% Constraints
+% for ct = 1:nu
+%     nlobj.MV(ct).Min = -pi;
+%     nlobj.MV(ct).Max =  pi;
+% end
+% 
+% for ct = 9:ny
+%     nlobj.OV(ct).Min = -pi;
+%     nlobj.OV(ct).Max =  pi;
+% end
 
-for ct = 1:nu
-    nlobj.MV(ct).Min = -pi;
-    nlobj.MV(ct).Max =  pi;
-end
-
-for ct = 9:ny
-    nlobj.OV(ct).Min = -pi;
-    nlobj.OV(ct).Max =  pi;
-end
-
+%% Denso definition, by DQ Robotics
 denso = DQ_DENSO;
 
 theta = [0, 0, -pi/2, 0, 0, 0]';
@@ -43,9 +43,8 @@ u0 = [0, 0, 0, 0, 0, 0]';
 
 validateFcns(nlobj,x0,u0);
 
-%% Defining setpoint
+%% Setpoint
 position = [0.2, 0, 0.2];
-
 phi = pi;
 
 n_vec = [0, 1, 0];
@@ -61,8 +60,8 @@ p = DQ([0, position(1), position(2), position(3)]);
 
 xd = r + 1/2 * DQ.E * p * r;
 
-%% Computing setpoint trajectory
-setting_time = 5;
+%% Computing Setpoint Trajectory
+setting_time = 2;
 
 setpoint = zeros(14, setting_time/Ts); 
 for k = 1:setting_time/Ts
@@ -73,12 +72,11 @@ for k = 1:nlobj.PredictionHorizon*2
     setpoint(:, setting_time/Ts + k) = setpoint(:, setting_time/Ts);
 end
 
-% setpoint = [x0 setpoint];
+setpoint = [x0 setpoint];
 
 %% Closed loop control law
 
 x = x0;
-y = x;
 data = [vec8(xm)', theta'];
 
 dtheta = zeros(6, 1);
@@ -87,117 +85,25 @@ k = 1;
 
 mv = zeros(1,6);
 
-% nloptions = nlmpcmoveopt;
-% nloptions.Parameters = {Ts};
-
-while (k < setting_time/Ts)
+while (k <= setting_time/Ts)
     
-%     [mv, nloptions, info] = nlmpcmove(nlobj, x, dtheta', setpoint', [], nloptions);
     [mv, ~, info] = nlmpcmove(nlobj, x, dtheta, setpoint(:, k+1:k+nlobj.PredictionHorizon)', []);
-%     [mv, ~, info] = nlmpcmove(nlobj, x, dtheta, kron(ones(nlobj.PredictionHorizon,1),setpoint(:, end)'), []);
-%     mv = nlmpcmove(nlobj, x, dtheta, kron(ones(nlobj.PredictionHorizon,1),setpoint(:, end)'), []);
-%     dtheta = nlmpcmove(nlobj, x, dtheta, setpoint(:, k:k+nlobj.PredictionHorizon)', [], nloptions)
-%     disp(setpoint(:, k:k+nlobj.PredictionHorizon)');
 
     dtheta = info.MVopt(1,:)';
-%     dtheta = mv
-%     mv
     theta = theta + dtheta * Ts;
     
-%     x = x + densoStateFunction(x, dtheta)*Ts
     x = [vec8(denso.fkm(theta)); theta];
     
-%     t = 1:nlobj.PredictionHorizon+1;
-%     
-%     subplot(4, 2, 1);
-%     plot(t, info.Xopt(:, 1));
-%     hold on
-%     plot(t, setpoint(1, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_1}', 'x_{d_1}');
-%     xlim([0, t(end)]);
-%     hold off
-% 
-%     subplot(4, 2, 2);
-%     plot(t, info.Xopt(:, 2));
-%     hold on
-%     plot(t, setpoint(2, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_2}', 'x_{d_2}');
-%     xlim([0, t(end)]);
-%     ylim([-.005, .005]);
-%     hold off
-% 
-%     subplot(4, 2, 3);
-%     plot(t, info.Xopt(:, 3));
-%     hold on
-%     plot(t, setpoint(3, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_3}', 'x_{d_3}');
-%     xlim([0, t(end)]);
-%     ylim([-2, 2]);
-%     hold off
-% 
-%     subplot(4, 2, 4);
-%     plot(t, info.Xopt(:, 4));
-%     hold on
-%     plot(t, setpoint(4, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_4}', 'x_{d_4}');
-%     xlim([0, t(end)]);
-%     ylim([-.005, .005]);
-%     hold off
-% 
-%     subplot(4, 2, 5);
-%     plot(t, info.Xopt(:, 5));
-%     hold on
-%     plot(t, setpoint(5, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_5}', 'x_{d_5}');
-%     xlim([0, t(end)]);
-%     ylim([-.005, .005]);
-%     hold off
-% 
-%     subplot(4, 2, 6);
-%     plot(t, info.Xopt(:, 6));
-%     hold on
-%     plot(t, setpoint(6, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_6}', 'x_{d_6}');
-%     xlim([0, t(end)]);
-%     hold off
-% 
-%     subplot(4, 2, 7);
-%     plot(t, info.Xopt(:, 7));
-%     hold on
-%     plot(t, setpoint(7, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_7}', 'x_{d_7}');
-%     xlim([0, t(end)]);
-%     ylim([-.005, .005]);
-%     hold off
-% 
-%     subplot(4, 2, 8);
-%     plot(t, info.Xopt(:, 8));
-%     hold on
-%     plot(t, setpoint(8, 1:21));
-%     xlabel("t(s)");
-%     legend('x_{m_8}', 'x_{d_8}');
-%     xlim([0, t(end)]);
-%     hold off
-
     plot(denso, theta');
     drawnow;
     
-    if (k<size(setpoint,2))
-        k = k+1;
-    end
+    k = k+1;
     
     data = [data; x'];
 end
 
 %% Plotting performance
-t = Ts:Ts:k*Ts;
+t = 0:Ts:(k-1)*Ts;
 
 if k < size(setpoint, 2)
     setpoint = setpoint(:, 1:k);
@@ -206,6 +112,8 @@ elseif k > size(setpoint, 2)
         setpoint(:, i) = setpoint(:, end);
     end
 end
+
+% setpoint = [x0 setpoint];
 
 subplot(4, 2, 1);
 plot(t, data(:, 1));
