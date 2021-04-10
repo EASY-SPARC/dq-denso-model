67c541d93916a0ecb90d0f39948de4707ab017a1
Vangasse <vangasse@ic.ufal.br>
2021-04-10 20:27:40 -0300

    Translational Geometric Jacobian deduction

--- matlab/FKMQ_Denso/denso_fkm.m
+++ matlab/FKMQ_Denso/denso_fkm.m
@@ -1,8 +1,7 @@
+%% Load Parameters
 syms theta1 theta2 theta3 theta4 theta5 theta6
 
 thetas = [theta1 theta2 theta3 theta4 theta5 theta6];
-
-% thetas = [pi/4 pi/4 pi/4 pi/4 pi/4 pi/4];
 
 denso_DH_theta=  [0,pi/2,-pi/2,0,0,0];
 denso_DH_d =     [0.125,0,0,0.210,0,0.070];
@@ -13,45 +12,19 @@ r_z_thetas = [];
 r_x_alphas = [];
 x = [];
 
+%% Quaternion Rotation Jacobian
+
 for i = 1:6
     r_z_thetas = [r_z_thetas rotation_z(thetas(i) + denso_DH_theta(i))];
     r_x_alphas = [r_x_alphas rotation_x(denso_DH_alpha(i))];
     x = [x hemi(r_z_thetas(:,i))*r_x_alphas(:,i)];
 end
 
-% r_z_theta1 = rotation_z(thetas(1));
-% r_z_theta2 = rotation_z(thetas(2) + pi/2);
-% r_z_theta3 = rotation_z(thetas(3) - pi/2);
-% r_z_theta4 = rotation_z(thetas(4));
-% r_z_theta5 = rotation_z(thetas(5));
-% r_z_theta6 = rotation_z(thetas(6));
-% 
-% r_x_alpha1 = rotation_x(pi/2);
-% r_x_alpha2 = rotation_x(0);
-% r_x_alpha3 = rotation_x(-pi/2);
-% r_x_alpha4 = rotation_x(pi/2);
-% r_x_alpha5 = rotation_x(-pi/2);
-% r_x_alpha6 = rotation_x(0);
-% 
-% x0 = hemi(r_z_theta1)*r_x_alpha1;
-% x1 = hemi(r_z_theta2)*r_x_alpha2;
-% x2 = hemi(r_z_theta3)*r_x_alpha3;
-% x3 = hemi(r_z_theta4)*r_x_alpha4;
-% x4 = hemi(r_z_theta5)*r_x_alpha5;
-% x5 = hemi(r_z_theta6)*r_x_alpha6;
-
 x_0_i = x(:,1);
 
 for i = 2:6
     x_0_i = [x_0_i hemi(x_0_i(:,i-1))*x(:,i)];
 end
-
-% x_0_1 = x(:,1);
-% x_0_2 = hemi(x_0_1)*x(:,2);
-% x_0_3 = hemi(x_0_2)*x(:,3);
-% x_0_4 = hemi(x_0_3)*x(:,4);
-% x_0_5 = hemi(x_0_4)*x(:,5);
-% x_0_6 = hemi(x_0_5)*x(:,6);
 
 z = [];
 J = [];
@@ -69,13 +42,75 @@ for i = 1:5
     J = [J hemi(z(:,end))*x_0_i(:,end)];
 end
 
-% for i = 1:4
-%     for j = 1:6
-%         J(i, j) = simplify(J(i, j));
+J = simplify(J);
+
+%% Geometric Translation Jacobian
+
+T_0_1 = geometric_rotation_z(theta1+denso_DH_theta(1)) * geometric_translation_z(denso_DH_d(1)) * geometric_translation_x(denso_DH_a(1)) * geometric_rotation_x(denso_DH_alpha(1));
+T_1_2 = geometric_rotation_z(theta2+denso_DH_theta(2)) * geometric_translation_z(denso_DH_d(2)) * geometric_translation_x(denso_DH_a(2)) * geometric_rotation_x(denso_DH_alpha(2));
+T_2_3 = geometric_rotation_z(theta3+denso_DH_theta(3)) * geometric_translation_z(denso_DH_d(3)) * geometric_translation_x(denso_DH_a(3)) * geometric_rotation_x(denso_DH_alpha(3));
+T_3_4 = geometric_rotation_z(theta4+denso_DH_theta(4)) * geometric_translation_z(denso_DH_d(4)) * geometric_translation_x(denso_DH_a(4)) * geometric_rotation_x(denso_DH_alpha(4));
+T_4_5 = geometric_rotation_z(theta5+denso_DH_theta(5)) * geometric_translation_z(denso_DH_d(5)) * geometric_translation_x(denso_DH_a(5)) * geometric_rotation_x(denso_DH_alpha(5));
+T_5_e = geometric_rotation_z(theta6+denso_DH_theta(6)) * geometric_translation_z(denso_DH_d(6)) * geometric_translation_x(denso_DH_a(6)) * geometric_rotation_x(denso_DH_alpha(6));
+
+T_0_2 = T_0_1*T_1_2;
+T_0_3 = T_0_2*T_2_3;
+T_0_4 = T_0_3*T_3_4;
+T_0_5 = T_0_4*T_4_5;
+T_0_e = T_0_5*T_5_e;
+
+% T = [];
+% for i = 1:6
+%     T = [T geometric_rotation_z(thetas(i)+denso_DH_theta(i))*geometric_translation_z(denso_DH_d(i))*geometric_translation_x(denso_DH_a(i))*geometric_rotation_x(denso_DH_alpha(i))];
+%     if i > 1
+%         T(:,4*i-3:4*i) = T(:,4*(i-1)-3:4*(i-1))*T(:,4*i-3:4*i);
 %     end
 % end
+% 
+% T = simplify(T);
 
-J = simplify(J)
+z_0 = [0;0;1];
+z_1 = T_0_1(1:3, 3);
+z_2 = T_0_2(1:3, 3);
+z_3 = T_0_3(1:3, 3);
+z_4 = T_0_4(1:3, 3);
+z_5 = T_0_5(1:3, 3);
+
+p_0 = [0;0;0];
+p_1 = T_0_1(1:3, 4);
+p_2 = T_0_2(1:3, 4);
+p_3 = T_0_3(1:3, 4);
+p_4 = T_0_4(1:3, 4);
+p_5 = T_0_5(1:3, 4);
+p_e = T_0_e(1:3, 4);
+
+% z = [0;0;1];
+% p = [0;0;0;1];
+% p_e = T(:, end-3:end)*p;
+% p_e = p_e(1:3);
+% 
+% for i = 1:5
+%     z = [z T(1:3,4*i-3:4*i-1)*z(:,1)];
+%     p = [p T(:,4*i-3:4*i)*p(:,1)];
+% end
+% p = p(1:3,:);
+
+J_p1 = cross(z_0', (p_e-p_0)')';
+J_p2 = cross(z_1', (p_e-p_1)')';
+J_p3 = cross(z_2', (p_e-p_2)')';
+J_p4 = cross(z_3', (p_e-p_3)')';
+J_p5 = cross(z_4', (p_e-p_4)')';
+J_p6 = cross(z_5', (p_e-p_5)')';
+
+J = [J_p1 J_p2 J_p3 J_p4 J_p5 J_p6; J];
+
+% J_g = [];
+% for i = 1:6
+%     J_g = [J_g cross(z(:,i)', (p_e-p(:,i))')'];
+% end
+% J_g = simplify(J_g);
+% digits(5);
+% J = [vpa(J_g, 4);J];
 
 
 
